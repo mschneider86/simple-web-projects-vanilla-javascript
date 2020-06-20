7a5295e700cbe49653ecb1f9f108bbaad3722043
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-20 20:20:31 -0300

    Added match passwords function

--- script.js
+++ script.js
@@ -56,6 +56,13 @@ function checkRequired(inputArr) {
   });
 }
 
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
 //Get fieldname
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -69,4 +76,5 @@ form.addEventListener('submit', (e) => {
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
   checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
