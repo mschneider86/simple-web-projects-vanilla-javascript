915f60cddc4878eebef7c962ae38e5873a6c8fae
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-21 16:44:48 -0300

    added mthods structure

--- 03-custom-video-player/script.js
+++ 03-custom-video-player/script.js
@@ -4,6 +4,31 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// Play & pause video
+function toggleVideoStatus() {
+  return true;
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+  return true;
+}
+
+// Update progress & timestamp
+function updateProgress() {
+  return true;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+  return true;
+}
+
+// Stop video
+function stopVideo() {
+  return true;
+}
+
 // Event listeners
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('play', updatePlayIcon);
