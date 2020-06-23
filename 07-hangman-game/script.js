f69b9fbfa5ce4cfcf2f988f7fbc9396549905430
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-22 21:17:26 -0300

    Added logic to display body parts for each error

--- 07-hangman-game/script.js
+++ 07-hangman-game/script.js
@@ -46,6 +46,16 @@ function updateWrongLettersEl() {
     ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
     ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
   `;
+
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
 }
 
 // Show notification
