2bb233a9dffe81cc991e197bc9fb253a7352363e
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-25 20:01:42 -0300

    Update timer bonus based on chosen difficulty

--- 12-typing-game/script.js
+++ 12-typing-game/script.js
@@ -106,7 +106,13 @@ text.addEventListener('input', (e) => {
     // Clear
     e.target.value = '';
 
-    time += 5;
+    if (difficulty === 'hard') {
+      time += 2;
+    } else if (difficulty === 'medium') {
+      time += 3;
+    } else {
+      time += 5;
+    }
 
     updateTime();
   }
