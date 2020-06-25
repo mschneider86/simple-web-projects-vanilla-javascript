c4261f039d52d42822bc7f2539bacf3bb98d246e
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-24 21:07:59 -0300

    Added methods to play and pause the music

--- 10-music-player/script.js
+++ 10-music-player/script.js
@@ -1,4 +1,4 @@
-const musicContainer = document.getElementById('music_container');
+const musicContainer = document.getElementById('music-container');
 
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
@@ -25,3 +25,32 @@ function loadSong(song) {
   audio.src = `music/${song}.mp3`;
   cover.src = `images/${song}.jpg`;
 }
+
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
