7705661f3d86d0d773e8354d336847b340d7f6e6
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-28 19:34:23 -0300

    Added method to display total of existing cards

--- 14-flashcard-app/script.js
+++ 14-flashcard-app/script.js
@@ -67,6 +67,13 @@ function createCard(data, index) {
   cardsEl.push(card);
 
   cardsContainer.appendChild(card);
+
+  updateCurrentText();
+}
+
+// Show number of cards
+function updateCurrentText() {
+  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
 
 createCards();
