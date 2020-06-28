cceee1859458be80ff9b3c54db0024cf7f9ca09b
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-28 20:02:21 -0300

    Added events to show/hide add card container

--- 14-flashcard-app/script.js
+++ 14-flashcard-app/script.js
@@ -137,3 +137,9 @@ nextBtn.addEventListener('click', showNextCard);
 
 // Previous Button
 prevBtn.addEventListener('click', showPreviousCard);
+
+// Show add container
+showBtn.addEventListener('click', () => addContainer.classList.add('show'));
+
+// Hide add container
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
