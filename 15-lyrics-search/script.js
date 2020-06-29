fbd734e399409c1f43e904367a90089260d6175d
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-29 19:39:04 -0300

    replaced search using map

--- 15-lyrics-search/script.js
+++ 15-lyrics-search/script.js
@@ -15,23 +15,37 @@ async function searchSongs(term) {
 
 // Show song and artist in DOM
 function showData(data) {
-  let output = '';
+  //let output = '';
 
-  data.data.forEach((song) => {
-    output += `
-      <li>
-        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-        <button class="btn" data-artist="${song.artist.name}"
-      data-songtitle="${song.title}">Get Lyrics</button>
-      </li>
-      
-    `;
-  });
+  // data.data.forEach((song) => {
+  //   output += `
+  //     <li>
+  //       <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //       <button class="btn" data-artist="${song.artist.name}"
+  //     data-songtitle="${song.title}">Get Lyrics</button>
+  //     </li>
+
+  //   `;
+  // });
+
+  // result.innerHTML = `
+  //   <ul class="songs">
+  //   ${output}
+  //   </ul>
+  // `;
 
   result.innerHTML = `
-    <ul class="songs">
-    ${output}
-    </ul>
+  <ul class="songs">
+  ${data.data
+    .map(
+      (song) => `<li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+     <button class="btn" data-artist="${song.artist.name}"
+      data-songtitle="${song.title}">Get Lyrics</button>
+    </li>`
+    )
+    .join('')}
+  </ul>
   `;
 }
 
