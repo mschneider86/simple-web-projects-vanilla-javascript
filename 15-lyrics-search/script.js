fa161a5818c33ae332c8b4f339f43b85cc59f9a5
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-29 19:34:54 -0300

    Fixed error displaying result in header

--- 15-lyrics-search/script.js
+++ 15-lyrics-search/script.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-const result = document.getElementById('form');
+const result = document.getElementById('result');
 const more = document.getElementById('more');
 
 const baseURL = 'https://api.lyrics.ovh';
