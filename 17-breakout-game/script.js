f6c41c03e17a25b06d429d9374a02529cb969c40
Matheus Schneider <matheus.ssm@gmail.com>
2020-07-02 19:01:29 -0300

    added paddle collision

--- 17-breakout-game/script.js
+++ 17-breakout-game/script.js
@@ -115,6 +115,15 @@ function moveBall() {
   if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
     ball.dy *= -1;
   }
+
+  // Paddle collision
+  if (
+    ball.x - ball.size > paddle.x &&
+    ball.x + ball.size < paddle.x + paddle.w &&
+    ball.y + ball.size > paddle.y
+  ) {
+    ball.dy = -ball.speed;
+  }
 }
 
 // Draw everything
