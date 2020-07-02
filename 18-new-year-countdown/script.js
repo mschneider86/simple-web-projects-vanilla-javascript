828414205c4d98873c2df8c32875d6e7542c746b
Matheus Schneider <matheus.ssm@gmail.com>
2020-07-02 19:47:31 -0300

    displayed the next year value on screen

--- 18-new-year-countdown/script.js
+++ 18-new-year-countdown/script.js
@@ -8,6 +8,10 @@ const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countdown time
 function updateCountdown() {
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
@@ -17,6 +21,7 @@ function updateCountdown() {
   const m = Math.floor(diff / 1000 / 60) % 60;
   const s = Math.floor(diff / 1000) % 60;
 
+  // Add values to DOM
   days.innerHTML = d;
   hours.innerHTML = h < 10 ? '0' + h : h;
   minutes.innerHTML = m < 10 ? '0' + m : m;
