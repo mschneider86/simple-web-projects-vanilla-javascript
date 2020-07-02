dbd6d0a2337d34173534e6cdc991f6ff4e70f126
Matheus Schneider <matheus.ssm@gmail.com>
2020-07-02 17:56:27 -0300

    added canvas context

--- 17-breakout-game/script.js
+++ 17-breakout-game/script.js
@@ -1,6 +1,8 @@
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
 // Event listeners
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
