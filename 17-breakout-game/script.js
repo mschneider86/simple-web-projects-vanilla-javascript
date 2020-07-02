613b1d2c4d8ae8ef53812fa6721fcb2f4b04b4c6
Matheus Schneider <matheus.ssm@gmail.com>
2020-07-02 18:17:16 -0300

    added function to create the bricks array

--- 17-breakout-game/script.js
+++ 17-breakout-game/script.js
@@ -6,6 +6,9 @@ const ctx = canvas.getContext('2d');
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 // Create ball props
 const ball = {
   x: canvas.width / 2,
@@ -25,6 +28,27 @@ const paddle = {
   speed: 8,
   dx: 0,
 };
+
+// Create brick props
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+// Create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = i * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
 
 // Draw ball on canvas
 function drawBall() {
