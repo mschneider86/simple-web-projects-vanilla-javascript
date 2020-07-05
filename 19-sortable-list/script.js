9d97b3f7ae5fa8ebb49dc1a08009dce992b8bbed
Matheus Schneider <matheus.ssm@gmail.com>
2020-07-05 11:16:53 -0300

    added functiion to swap the items

--- 19-sortable-list/script.js
+++ 19-sortable-list/script.js
@@ -33,12 +33,12 @@ function createList() {
       listItem.setAttribute('data-index', index);
 
       listItem.innerHTML = `
-    <span class="number">${index + 1}</span>
-    <div class="draggable" draggable="true">
-      <p class="person-name">${person}</p>
-      <i class="fas fa-grip-lines"></i>
-    </div>
-    `;
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `;
 
       listItems.push(listItem);
 
@@ -46,6 +46,38 @@ function createList() {
     });
 
   addEventListeners();
+}
+
+function dragStart() {
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragEnter() {
+  this.classList.add('over');
+}
+
+function dragLeave() {
+  this.classList.remove('over');
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
 }
 
 function addEventListeners() {
