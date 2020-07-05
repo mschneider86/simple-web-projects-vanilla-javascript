7a67bca36f4f018846a06841b185c2cbfd080dc0
Matheus Schneider <matheus.ssm@gmail.com>
2020-07-05 12:12:59 -0300

    added function to check the spoken number

--- 20-speak-number-guess/script.js
+++ 20-speak-number-guess/script.js
@@ -17,7 +17,37 @@ function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
   writeMessage(msg);
-  //checkNumber(msg);
+  checkNumber(msg);
+}
+
+// Check msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  // Check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += '<div>That is not a valid number</div>';
+    return;
+  }
+
+  // Check in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+    return;
+  }
+
+  // Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div>GO LOWER</div>';
+  } else {
+    msgEl.innerHTML += '<div>GO HIGHER</div>';
+  }
 }
 
 // Write what user speaks
